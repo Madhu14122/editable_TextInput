2dd8cb14f54c6b565242260d4d801ea8c322778c
Madhu14122 <coolcoder14122@gmail.com>
2023-08-20 13:04:57 +0530

    first-commit

---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  background-color;#f5d0fe;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+  background-color:#f5d0fe;
+  `
+export const CardContainer = styled.div`
+  background-color: #ffffff;
+  padding: 30px;
+  height: 200px;
+  border-radius: 10px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+export const Heading = styled.h1`
+  font-size: 20px;
+  font-family: 'Roboto';
+`
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+export const Paragraph = styled.p`
+  font-size: 16px;
+  font-family: 'Roboto';
+  padding-right: 10px;
+`
+export const Input = styled.input`
+  height: 30px;
+  width: 200px;
+  outline: none;
+  margin-right: 20px;
+  padding: 10px;
+`
+export const Button = styled.button`
+  height: 30px;
+  width: 55px;
+  cursor: pointer;
+  background-color: #d946ef;
+  border-radius: 5px;
+  color: #ffffff;
+  border-width: 0px;
+  font-size: 13px;
+  font-family: 'Roboto';
+`
